c18105d5ba4b65af8d54650af7a2247f867fb327
Deyvid Popov <47706857+DeyvidBG@users.noreply.github.com>
2023-05-09 11:05:04 +0300

    eight_queens_dfs added with comments in English and German

--- eight_queens_dfs_de.py
+++ eight_queens_dfs_de.py
@@ -1,12 +1,15 @@
 def is_safe(board, row, col):
+    # Prüfe auf horizontale Angriffe
     for i in range(col):
         if board[row][i] == 1:
             return False
 
+    # Prüfe auf diagonale Angriffe oben links
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
+    # Prüfe auf diagonale Angriffe unten links
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -14,9 +17,11 @@ def is_safe(board, row, col):
     return True
 
 def solve_eight_queens_dfs(board, col):
+    # Wenn alle Königinnen platziert sind, ist das Problem gelöst
     if col >= len(board):
         return True
 
+    # Gehe durch alle Zeilen der aktuellen Spalte
     for i in range(len(board)):
         if is_safe(board, i, col):
             board[i][col] = 1
@@ -26,7 +31,6 @@ def solve_eight_queens_dfs(board, col):
 
     return False
 
-
 def print_board(board):
     for row in board:
         print(" ".join(str(cell) for cell in row))
@@ -34,7 +38,7 @@ def print_board(board):
 board = [[0 for _ in range(8)] for _ in range(8)]
 
 if solve_eight_queens_dfs(board, 0):
-    print("Solution found:")
+    print("Lösung gefunden:")
     print_board(board)
 else:
-    print("No solution found")
+    print("Keine Lösung gefunden")
